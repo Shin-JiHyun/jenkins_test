c0a06031b3c039bc9e92e0114139324d28cdc2bf
= <sjhyun0926@gmail.com>
2025-03-18 15:08:25 +0900

    jenkins docker test

--- src/main/java/com/example/jenkins/TestController.java
+++ src/main/java/com/example/jenkins/TestController.java
@@ -1,7 +1,14 @@
 package com.example.jenkins;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping
 @RestController
 public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
